cc29df9060f4b977ad7d0a8c330b858076bb6352
Macatuz <armando29@live.com>
2019-05-08 14:36:30 -0400

    refactoring due sort of bad sample

--- main.rb
+++ main.rb
@@ -1,25 +1,28 @@
 def bubble_sort(arr)
-  result = []
-  for i in 1..arr.count
-    result.push(minimun(arr))
-  end
-  return result
-end
-
-def minimun(arr)
-  p "array is #{arr}"
-  result = 1000
-  resultindex = 0
-
-  arr.each_with_index do |value, index|
-    if value < result
-      result = value
-      resultindex = index
+  while (!isSorted(arr))
+    for i in 0..arr.count - 1
+      break if i == arr.count - 1
+      Switch(i, i + 1, arr) if arr[i] > arr[i + 1]
     end
   end
+  return arr
+end
 
-  arr.delete_at(resultindex)
-  return result
+def Switch(indexA, indexB, arr)
+  valueA = arr[indexA]
+  valueB = arr[indexB]
+  arr[indexA] = valueB
+  arr[indexB] = valueA
+end
+
+def isSorted(arr)
+  isSorted = 0
+  for i in 0..(arr.count - 1)
+    next if i === 0
+    break if arr[i] < arr[i - 1]
+    isSorted += 1
+  end
+  return isSorted == arr.count - 1
 end
 
 p bubble_sort([4, 3, 78, 2, 0, 2])

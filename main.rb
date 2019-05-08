900ddc93f874b91f67de2880194c471b5bacfb21
Macatuz <armando29@live.com>
2019-05-08 15:26:25 -0400

    bubble_sort_by

--- main.rb
+++ main.rb
@@ -25,4 +25,31 @@ def isSorted(arr)
   return isSorted == arr.count - 1
 end
 
+def isStringSorted(arr)
+  isSorted = 0
+  for i in 0..(arr.count - 1)
+    next if i === 0
+    break if arr[i].length < arr[i - 1].length
+    isSorted += 1
+  end
+  return isSorted == arr.count - 1
+end
+
+def bubble_sort_by(arr)
+  if block_given?
+    while (!isStringSorted(arr))
+      for i in 0..(arr.count - 1)
+        next if i === 0
+        if yield(arr[i - 1], arr[i]) > 0
+          Switch(i - 1, i, arr)
+        end
+      end
+    end
+  end
+  p arr
+end
+
 p bubble_sort([4, 3, 78, 2, 0, 2])
+bubble_sort_by(["hi", "hello", "hey"]) do |a, b|
+  a.length - b.length
+end
